d5312a5f352ed8cc72c360a7ef13180667dbcc24
furetur <furetur@gmail.com>
2021-06-09 00:20:49 +0500

    added operator tests

--- src/jex/operators.rs
+++ src/jex/operators.rs
@@ -96,3 +96,104 @@ pub fn print(value: JexValue) -> Result<JexValue, Exception> {
     println!("{}", value.to_output_string());
     Ok(JexValue::Null(JexNull))
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::jex::jex_values::values::JexValue;
+    use crate::jex::operators::{divide, equal, greater, less, minus, multiply, negate, not, plus};
+
+    #[test]
+    fn plus_should_add_two_ints() {
+        assert_eq!(
+            JexValue::Int(100),
+            plus(JexValue::Int(80), JexValue::Int(20)).unwrap()
+        );
+    }
+
+    #[test]
+    fn plus_should_add_two_strings() {
+        let left = JexValue::from_string("A".to_string());
+        let right = JexValue::from_string("BC".to_string());
+        let expected = JexValue::from_string("ABC".to_string());
+
+        assert_eq!(expected, plus(left, right).unwrap());
+    }
+
+    #[test]
+    fn minus_should_subtract_ints() {
+        assert_eq!(
+            JexValue::Int(-50),
+            minus(JexValue::Int(20), JexValue::Int(70)).unwrap()
+        );
+    }
+
+    #[test]
+    fn multiply_should_multiply_ints() {
+        assert_eq!(
+            JexValue::Int(-50),
+            multiply(JexValue::Int(2), JexValue::Int(-25)).unwrap()
+        );
+    }
+
+    #[test]
+    fn divide_should_divide_ints() {
+        assert_eq!(
+            JexValue::Int(-33),
+            divide(JexValue::Int(100), JexValue::Int(-3)).unwrap()
+        );
+    }
+
+    #[test]
+    fn negate_should_negate_int() {
+        assert_eq!(JexValue::Int(-33), negate(JexValue::Int(33)).unwrap());
+    }
+
+    #[test]
+    fn not_should_negate_bool() {
+        assert_eq!(JexValue::Bool(true), not(JexValue::Bool(false)).unwrap());
+    }
+
+    #[test]
+    fn two_same_ints_should_be_equal() {
+        assert_eq!(
+            JexValue::Bool(true),
+            equal(JexValue::Int(100), JexValue::Int(100)).unwrap()
+        );
+    }
+
+    #[test]
+    fn two_different_ints_should_be_not_equal() {
+        assert_eq!(
+            JexValue::Bool(false),
+            equal(JexValue::Int(10), JexValue::Int(100)).unwrap()
+        );
+    }
+
+    #[test]
+    fn two_same_strings_should_be_equal() {
+        assert_eq!(
+            JexValue::Bool(true),
+            equal(
+                JexValue::from_string("aab".to_string()),
+                JexValue::from_string("aab".to_string())
+            )
+            .unwrap()
+        );
+    }
+
+    #[test]
+    fn test_10_should_be_greater_than_minus_100() {
+        assert_eq!(
+            JexValue::Bool(true),
+            greater(JexValue::Int(10), JexValue::Int(-100)).unwrap()
+        );
+    }
+
+    #[test]
+    fn test_10_should_be_less_than_50() {
+        assert_eq!(
+            JexValue::Bool(true),
+            less(JexValue::Int(10), JexValue::Int(50)).unwrap()
+        );
+    }
+}
